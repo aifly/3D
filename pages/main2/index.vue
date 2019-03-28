43fd7525bf8913169e3663d88495e2e6d82d7f8c
= <aifly_xc@163.com>
2019-03-28 18:31:38 +0800

    a

--- pages/main2/index.vue
+++ pages/main2/index.vue
@@ -10,24 +10,6 @@
 					</div>
 				</div>
 			</div>
-			<div class="candle out" >
-				<div class="candle-body"></div>
-				<!-- 火焰 -->
-				<div class="candle-flame"></div>
-				<!-- 烟雾 -->
-				 <div class="candle-smoke">
-					<span class="s0"></span>
-					<span class="s1"></span>
-					<span class="s2"></span>
-					<span class="s3"></span>
-					<span class="s4"></span>
-					<span class="s5"></span>
-					<span class="s6"></span>
-					<span class="s7"></span>
-					<span class="s8"></span>
-					<span class="s9"></span>
-				</div>
-			</div>
 		</div>
 	</transition>
 </template>
